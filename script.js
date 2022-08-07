fb1c32c82b7b967150dc1c64bcfaeb6df88d6600
serioznuh <ooopps404@gmail.com>
2022-08-07 23:21:54 +0300

    Add a redesign to make grid white

--- script.js
+++ script.js
@@ -74,16 +74,46 @@ function mouseover() {
   cells.forEach(cell => {
     let count = 0;
     cell.addEventListener('mouseover', event => {
-      if (count === 0) event.target.style.background = '#e7e7e5';
-      if (count === 1) event.target.style.background = '#cfcfcc';
-      if (count === 2) event.target.style.background = '#b7b7b2';
-      if (count === 3) event.target.style.background = '#9f9f99';
-      if (count === 4) event.target.style.background = '#88877f';
-      if (count === 5) event.target.style.background = '#706f66';
-      if (count === 6) event.target.style.background = '#58574c';
-      if (count === 7) event.target.style.background = '#403f32';
-      if (count === 8) event.target.style.background = '#282719';
-      if (count === 9) event.target.style.background = '#111000';
+      if (count === 0) {
+        event.target.style.background = '#e7e7e5';
+        event.target.style.borderColor = '#e7e7e5';
+      }
+      if (count === 1) {
+        event.target.style.background = '#cfcfcc';
+        event.target.style.borderColor = '#cfcfcc';
+      }
+      if (count === 2) {
+        event.target.style.background = '#b7b7b2';
+        event.target.style.borderColor = '#b7b7b2';
+      }
+      if (count === 3) {
+        event.target.style.background = '#9f9f99';
+        event.target.style.borderColor = '#9f9f99';
+      }
+      if (count === 4) {
+        event.target.style.background = '#88877f';
+        event.target.style.borderColor = '#88877f';
+      }
+      if (count === 5) {
+        event.target.style.background = '#706f66';
+        event.target.style.borderColor = '#706f66';
+      }
+      if (count === 6) {
+        event.target.style.background = '#58574c';
+        event.target.style.borderColor = '#58574c';
+      }
+      if (count === 7) {
+        event.target.style.background = '#403f32';
+        event.target.style.borderColor = '#403f32';
+      }
+      if (count === 8) {
+        event.target.style.background = '#282719';
+        event.target.style.borderColor = '#282719';
+      }
+      if (count === 9) {
+        event.target.style.background = '#111000';
+        event.target.style.borderColor = '#111000';
+      }
     });
     cell.addEventListener('mouseout', () => {
       count++;
@@ -95,7 +125,7 @@ function resetGrid() {
   mouseover();
   const cells = document.querySelectorAll('.cell');
   cells.forEach(cell => {
-    cell.style.background = '#f2f2f2';
+    cell.style.background = '#ffffff';
   });
 }
 
@@ -104,6 +134,7 @@ function changeCellBackgroundColor(color) {
   cells.forEach(cell => {
     cell.addEventListener('mouseover', event => {
       event.target.style.background = color.target.value;
+      event.target.style.borderColor = color.target.value;
     });
   });
 }
